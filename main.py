20774c4a5d80c3c71448d9c07c0fd52a56ea8d6e
Joshua Schmucker <joshua.schmucker@gmail.com>
2021-10-28 22:22:17 +0200

    Added some information parsing from json file

--- main.py
+++ main.py
@@ -19,14 +19,41 @@ def get_recipes(recipepath):
 
 recipes = get_recipes(mypath)
 
-def get_name(recipe_path):
+
+def get_values(recipe_path):
     f = open(recipe_path, "r")
     lines = f.readlines()
-    print(lines)
     recipe_dict = json.loads(lines[0])
-    print("Name: " + recipe_dict['name'])
+    get_name(recipe_dict)
+    get_servings(recipe_dict)
+    get_ingredients(recipe_dict)
+    get_instructions(recipe_dict)
+    get_keywords(recipe_dict)
+
+
+def get_name(recipe):
+    print("Name: " + recipe['name'])
+
+
+def get_servings(recipe):
+    print("Servings: " + str(recipe['recipeYield']))
+
+
+def get_ingredients(recipe):
+    print("Ingredients: ")
+    print(recipe['recipeIngredient'])
+
+
+def get_instructions(recipe):
+    print("Instructions: ")
+    print(recipe['recipeInstructions'])
+
+
+def get_keywords(recipe):
+    print("Keywords: ")
+    print(recipe['keywords'])
 
 
 for r in recipes:
     print(r)
-    get_name(r)
+    get_values(r)
