139058ab1df2c9668e022862fec6edaa1fcddad8
Joshua Schmucker <joshua.schmucker@gmail.com>
2021-10-30 22:33:15 +0200

    Update

--- controller/mw_controller.py
+++ controller/mw_controller.py
@@ -1,3 +1,5 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QCheckBox, QVBoxLayout
 from controller.rc_controller import RvController
 from os.path import split
 from view import recipe_button as rb
@@ -6,6 +8,7 @@ import os
 
 
 class MwController:
+    label_filter = []
     
     
     def __init__(self, model, window):
@@ -15,6 +18,7 @@ class MwController:
 
         self.model.load_recipes()
         self.read_recipes()
+        self.create_checkboxes()
         self.window.backButton.clicked.connect(self.open_recipe_list)
 
 
@@ -30,20 +34,74 @@ class MwController:
 
 
     def read_recipes(self):
-        recipes = self.model.get_recipes()
+        self.recipes = self.model.get_recipes()
         self.categories = []
         self.nahrung = []
         self.kohlehydrate = []
-        for r in recipes:
+        for r in self.recipes:
             rd = self.model.get_recipe_dict(r)
             self.categories = self.categories + self.model.get_kategorien(rd)
             self.nahrung = self.nahrung + self.model.get_nahrung(rd)
             self.kohlehydrate = self.kohlehydrate + self.model.get_kohlehydrat(rd)
             self.window.add_recipe(self.create_recipe_button(r, rd))
+        self.categories = sorted(set(self.categories))
+        self.nahrung = sorted(set(self.nahrung))
+        self.kohlehydrate = sorted(set(self.kohlehydrate))
+        self.label_filter = self.categories + self.nahrung + self.kohlehydrate
         print(self.categories)
         print(self.nahrung)
         print(self.kohlehydrate)
         self.window.recipeList.layout().addStretch()
+
+
+    def reload_recipes(self):
+        for r in self.recipes:
+            rd = self.model.get_recipe_dict(r)
+            labels = self.model.get_kategorien(rd) + self.model.get_nahrung(rd) + self.model.get_kohlehydrat(rd)
+            for l in labels:
+                if l in self.label_filter:
+                    for b in self.window.get_recipe_buttons():
+                        print(b.text())
+
+
+    def filter_label(self, state, label):
+        if QtCore.Qt.Checked == state:
+            print("Checked")
+            print(self.label_filter)
+            if label not in self.label_filter:
+                self.label_filter.append(label)
+            print(self.label_filter)
+        else:
+            print("Unchecked")
+            print(self.label_filter)
+            if label in self.label_filter:
+                self.label_filter.remove(label)
+            print(self.label_filter)
+        self.reload_recipes()
+
+
+    def create_checkboxes(self):
+        for c in self.categories:
+            cb = QCheckBox(c.split('_')[1])
+            cb.categorie = c
+            if c in self.label_filter:
+                cb.setChecked(True)
+            cb.stateChanged.connect(lambda s, l=c: self.filter_label(s, l))
+            self.window.kategorieGroupBox.layout().addWidget(cb)
+        for n in self.nahrung:
+            cb = QCheckBox(n.split('_')[1])
+            cb.nahrung = n
+            if n in self.label_filter:
+                cb.setChecked(True)
+            cb.stateChanged.connect(lambda s, l=n: self.filter_label(s, l))
+            self.window.nahrungGroupBox.layout().addWidget(cb)
+        for k in self.kohlehydrate:
+            cb = QCheckBox(k.split('_')[1])
+            cb.kohlehydrat = k
+            if k in self.label_filter:
+                cb.setChecked(True)
+            cb.stateChanged.connect(lambda s, l=k: self.filter_label(s, l))
+            self.window.kohlehydrateGroupBox.layout().addWidget(cb)
 
 
     def get_image_path(self, recipe_path):
--- view/main_window.py
+++ view/main_window.py
@@ -4,13 +4,19 @@ from PyQt5.QtWidgets import QLabel, QMainWindow
 
 
 class MainWindow(QMainWindow):
+    recipe_buttons = []
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("view/main_window.ui", self)
 
 
     def add_recipe(self, recipe_button):
+        self.recipe_buttons.append(recipe_button)
         self.recipeList.layout().addWidget(recipe_button)
+
+
+    def get_recipe_buttons(self):
+        return self.recipe_buttons
 
 
     def set_image(self, image_path):
